cbd4cf1f1fc7bdb67000d6a06ceee9b1b3a25d52
Mehdi Abdollahi <mehdi7007@gmail.com>
2026-08-11 19:10:23 +0330

    fixing

--- config/settings.py
+++ config/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = []
 
 
@@ -129,8 +129,8 @@ REST_FRAMEWORK = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://red-d8ppdhs8aovs73becnkg:6379",
-        # "LOCATION": "redis://127.0.0.1:6379",
+        # "LOCATION": "redis://red-d8ppdhs8aovs73becnkg:6379",
+        "LOCATION": "redis://127.0.0.1:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
